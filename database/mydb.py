7147688ed37aa7254d4dcae9c96b67e52c7fff92
Rustambek97 <r.baltayev9997@gmail.com>
2024-03-20 17:19:41 +0500

    first commit

---
+++ database/mydb.py
@@ -0,0 +1,88 @@
+import mysql.connector
+
+class Database:
+    def __init__(self):
+        self.db = None
+
+    def ulanish(self):
+        return mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='Rustam123456b',
+            database='ice_cream'
+        )
+
+    def ishlatish(self, sql, fetchall=False, fetchone=False, commit=False):
+        self.db = self.ulanish()
+        cursor = self.db.cursor()
+        cursor.execute(sql)
+        data = None
+        if fetchall:
+            data = cursor.fetchall()
+        elif fetchone:
+            data = cursor.fetchone()
+        elif commit:
+            self.db.commit()
+
+        self.db.close()
+        return data
+
+    def hodimlar(self):
+        sql = "select * from hodimlar"
+        return self.ishlatish(sql, fetchall=True)
+
+    def muzqaymoqlar(self):
+        sql = "select * from muzqaymoqlar"
+        return self.ishlatish(sql, fetchall=True)
+
+    def mijozlar(self):
+        sql = "select * from mijozlar"
+        return self.ishlatish(sql, fetchall=True)
+
+    def hodim(self, id):
+        sql = f"select * from hodimlar where id={id}"
+        return self.ishlatish(sql, fetchone=True)
+
+    def hodimqushish(self, ism, familiya, tel, ish, karta):
+        sql = f"""
+        insert into hodimlar(ism, familiya, tel_raqam, ish_boshlagan, karta_raqam)
+        values ('{ism}', '{familiya}', '{tel}', '{ish}', '{karta}')
+        """
+        self.ishlatish(sql, commit=True)
+
+    def muzqaymoqturlari(self):
+        sql = "SELECT * FROM turi"
+        return self.ishlatish(sql, fetchall=True)
+
+    def muzqaymoqqushish(self, nomi, narxi, turi_id, ishlab_chiq_sana, yaroqlilik_muddati, soni):
+        sql = f"""
+        INSERT INTO muzqaymoqlar(nomi, narxi, turi_id, ishlab_chiq_sana, yaroqlilik_muddati, soni)
+        VALUES
+        ('{nomi}', {narxi}, {turi_id}, '{ishlab_chiq_sana}', '{yaroqlilik_muddati}', {soni})
+        """
+        self.ishlatish(sql, commit=True)
+
+    def hodimTahrir(self,id, ism, familiya, tel, ish_boshlash, karta):
+        sql = f"""
+        UPDATE hodimlar SET ism='{ism}', 
+        familiya='{familiya}', 
+        tel_raqam='{tel}', 
+        ish_boshlagan='{ish_boshlash}', 
+        karta_raqam='{karta}'
+        WHERE id = {id}
+        """
+        self.ishlatish(sql, commit=True)
+
+    def hodimid(self, id):
+        sql = f"select * from hodimlar where id = {id}"
+        return self.ishlatish(sql, fetchone=True)
+
+    def hodimdel(self, id):
+        sql = f"DELETE FROM hodimlar WHERE id = {id}"
+        self.ishlatish(sql, commit=True)
+
+    def search(self, matn):
+        sql = f"select * from hodimlar where ism LIKE '%{matn}%' or familiya LIKE '%{matn}%' "
+        return self.ishlatish(sql, fetchall=True)
+
+mydb = Database()
---
+++ main.py
@@ -0,0 +1,96 @@
+from flask import Flask, render_template, request, redirect
+from database.mydb import mydb
+from datetime import date
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/hodimlar', methods=['GET', 'POST'])
+def HodimlarRuyxati():
+    if request.method == 'GET':
+        hodimlar = mydb.hodimlar()
+        return render_template("hodimlar/hodimlar.html", hodimlar=hodimlar)
+    elif request.method == 'POST':
+        matn = request.form['qidiruv']
+        natijalar = mydb.search(matn)
+        return render_template("hodimlar/hodimlar.html", hodimlar=natijalar)
+
+
+@app.route('/muzqaymoqlar')
+def MuzqaymoqlarRuyxati():
+    muzqaymoqlar = mydb.muzqaymoqlar()
+    return render_template("muzqaymoqlar/muzqaymoqlar.html", muzqaymoqlar=muzqaymoqlar)
+
+@app.route('/mijozlar')
+def MijozlarRuyxati():
+    mijozlar = mydb.mijozlar()
+    return render_template("mijozlar/mijozlar.html", mijozlar=mijozlar)
+
+@app.route('/hodimlar/<id>')
+def TanlanganHodim(id):
+    a = mydb.hodim(int(id))
+    t = date.today() - a[4]
+    b = {
+        'hodim':a,
+        'vaqt':date.today(),
+        'farq':t.days
+    }
+    return render_template("hodimlar/hodim.html", b=b)
+
+@app.route('/hodimqushish', methods = ['GET', 'POST'])
+def Add():
+    if request.method == 'GET':
+        return render_template('hodimqushish.html')
+    elif request.method == 'POST':
+        ism = request.form['ism']
+        familiya = request.form['familiya']
+        tel = request.form['tel']
+        sana = request.form['ish_boshlash_sana']
+        karta = request.form['karta']
+
+        mydb.hodimqushish(ism, familiya, tel, sana, karta)
+
+        return redirect('/hodimlar')
+
+@app.route("/muzqaymoqqushish", methods=['GET', 'POST'])
+def muzqaymoqadd():
+    if request.method == 'GET':
+        turlar = mydb.muzqaymoqturlari()
+        return render_template("muzqaymoqlar/muzqaymoqqushish.html", turlar=turlar)
+    elif request.method == 'POST':
+        nomi = request.form['nomi']
+        narxi = request.form['narxi']
+        tur_id = request.form['turi']
+        chiqqan_sana = request.form['createdate']
+        tugash_sana = request.form['deletedate']
+        soni = request.form['soni']
+        mydb.muzqaymoqqushish(nomi, narxi, tur_id, chiqqan_sana, tugash_sana, soni)
+        return redirect("/muzqaymoqlar")
+
+@app.route("/hodim/tahrir/<id>", methods=['GET', 'POST'])
+def HodimTahrir(id):
+    if request.method == 'GET':
+        hodim = mydb.hodimid(id)
+        return render_template("hodimlar/hodimtahrirlash.html", hodim=hodim)
+    elif request.method == 'POST':
+        ism = request.form['ism']
+        familiya = request.form['familiya']
+        tel = request.form['tel']
+        ish_boshlash = request.form['ish_boshlash']
+        karta = request.form['karta']
+        mydb.hodimTahrir(id, ism, familiya, tel, ish_boshlash, karta)
+        return redirect("/hodimlar")
+
+@app.route("/hodim/del/<id>/", methods=['GET', 'POST'])
+def HodimDel(id):
+    if request.method == 'GET':
+        hodimlar = mydb.hodimid(id)
+        return render_template("hodimlar/hodimdel.html", hodimlar=hodimlar)
+    elif request.method == 'POST':
+        mydb.hodimdel(id)
+        return redirect("/hodimlar")
+
+if __name__=="__main__":
+    app.run(debug=True)
